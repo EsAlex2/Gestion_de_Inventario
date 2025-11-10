8dda53e639efec156082d1c41e13c1ac8554da30
EsAlex <alexmadrid326@gmail.com>
2025-11-10 16:08:34 -0400

    new commit

--- registro_usuario.php
+++ registro_usuario.php
@@ -30,20 +30,20 @@ require_once('top.php');
                         <p>Debe llenar todos los campos</p>
                     </div>
                     <div id="input" class="mb-4">
-                        <input type="text" id="nameFirts" class="input-registro" required placeholder="Nombres" maxlength="45">
-                        <input type="text" id="nameFirts" class="input-registro" required placeholder="Apellidos" maxlength="45" style="margin-top: 20px;">
+                        <input type="text" class="input-registro" required placeholder="Nombres" maxlength="45">
+                        <input type="text" class="input-registro" required placeholder="Apellidos" maxlength="45" style="margin-top: 20px;">
                     </div>
                     <div id="input" class="mb-4">
-                        <input type="email" id="email_telf" class="input-registro" required placeholder="Correo Electronico" maxlength="30">
-                        <input type="text" id="email_telf" class="input-registro" required placeholder="Telefono" maxlength="11" style="margin-top: 20px;">
-                        <input type="text" id="email_telf" class="input-registro" required placeholder="Telefono de Emergencia" maxlength="11" style="margin-top: 20px;">
+                        <input type="email" class="input-registro" required placeholder="Correo Electronico" maxlength="30">
+                        <input type="text"  class="input-registro" required placeholder="Telefono" maxlength="11" style="margin-top: 20px;">
+                        <input type="text"  class="input-registro" required placeholder="Telefono de Emergencia" maxlength="11" style="margin-top: 20px;">
                     </div>
                     <div id="input" class="mb-4">
-                        <input type="password" id="pass" class="input-registro" required maxlength="16">
-                        <input type="password" id="pass" class="input-registro" required maxlength="16" style="margin-top: 20px;">
+                        <input type="password" class="input-registro" required maxlength="16">
+                        <input type="password" class="input-registro" required maxlength="16" style="margin-top: 20px;">
                     </div>
                     <div id="date" class="mb-4">
-                        <input type="date" id="date_b" required style="margin-top: 20px;">
+                        <input type="date" required style="margin-top: 20px;">
                     </div>
                     <div class="button-registro">
                         <button type="submit" class="btn btn-outline-primary" style="width: 50%;">REGISTARSE</button>
@@ -53,6 +53,8 @@ require_once('top.php');
         </div>
     </div>
 </section>
+
+
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
