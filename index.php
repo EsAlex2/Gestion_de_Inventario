6bf1f52566373ddc4005bfaaa9f5bb60aa702aa9
alex jonfranc madrid marin <alexmadrid326@gmail.com>
2025-11-09 22:55:29 -0400

    Se agrego footer, formulario para envio de observaciones, seccion para menus en el footer

--- index.php
+++ index.php
@@ -139,31 +139,33 @@
 
     <!--colaboradores-->
     <section class="team">
-        <div class="container-fluid" style="margin: 50px;">
-            <div class="row">
-                <div id="colaboradores" class="col-12 text-center" style="padding-bottom: 100px">
-                    <h3 style="margin-bottom: 40px;">Colaboradores</h3>
-                    <p id="text-colaboladores">Nuestro equipo son un grupo de estudiantes universitarios con habilidades...</p>
-                </div>
-                <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
-                    <img src="./views/team1.png" alt="Colaborador 2" class="img-fluid colaborador-img">
-                    <p id="text-colaboladores">Alex J. Madrid</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis<br></p>
-                </div>
-                <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
-                    <img src="./views/team2.png" alt="Colaborador 2" class="img-fluid colaborador-img">
-                    <p id="text-colaboladores">Alex J. Madrid</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
-                </div>
-                <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
-                    <img src="./views/team3.png" alt="Colaborador 3" class="img-fluid colaborador-img">
-                    <p id="text-colaboladores">Alex J. Madrid</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
-                </div>
-                <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
-                    <img src="./views/yteam1.jpg" alt="Colaborador 4" class="img-fluid colaborador-img">
-                    <p id="text-colaboladores">Alex J. Madrid</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+        <div class="container-fluid" style="margin: 60px ">
+            <div class="fotos">
+                <div class="row">
+                    <div id="colaboradores" class="col-12 text-center" style="padding-bottom: 100px">
+                        <h3 style="margin-bottom: 40px;">Colaboradores</h3>
+                        <p id="text-colaboladores">Nuestro equipo son un grupo de estudiantes universitarios con habilidades...</p>
+                    </div>
+                    <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
+                        <img src="./views/team1.png" alt="Colaborador 2" class="img-fluid colaborador-img">
+                        <p id="text-colaboladores">Alex J. Madrid</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis<br></p>
+                    </div>
+                    <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
+                        <img src="./views/team2.png" alt="Colaborador 2" class="img-fluid colaborador-img">
+                        <p id="text-colaboladores">Alex J. Madrid</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                    </div>
+                    <div id="colaboradores_foto" class="col-6 col-md-3 text-center">
+                        <img src="./views/team3.png" alt="Colaborador 3" class="img-fluid colaborador-img">
+                        <p id="text-colaboladores">Alex J. Madrid</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                    </div>
+                    <div id="colaboradores_foto" class="col-6 col-md-3 text-center"">
+                        <img src="./views/team3.png" alt="Colaborador 3" class="img-fluid colaborador-img">
+                        <p id="text-colaboladores">Alex J. Madrid</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -173,10 +175,58 @@
     <!--Footer-->
     <section class="footer">
         <div id="footer" class="container-fluid">
-            <div class="contactos">
-                <h2>GesTec</h2>
-                <div class="icono-footer">
-                    <ion-icon name="navigate-circle-outline"></ion-icon>
+            <div class="row">
+                <div class="col-md-4 col-lg-4"> 
+                    <div class="contactos">
+                        <h2 style="text-align: start">Ges<span style="color: var(--text-color-span);">Tec</span></h2>
+                        <div class="contactos-footer">
+                            <p><strong>Direccion: </strong>Esquina Mijares, Avenida Oeste 3, Altagracia, Caracas 1010, 1010, Distrito Capital</p>
+                            <p><strong>Correo: </strong><a href="#" style="text-decoration: none; color: white">alexmadrid326@gmail.com</a></p>
+                            <p><strong>Telefono: </strong>+58 414 3770143</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4"> 
+                    <div class="contactos">
+                        <h5 style="padding-left: 28px">Menus</h5>
+                        <ul class="menus">
+                            <li><a id="link-menus" href="#">Home</a></li>
+                            <li><a id="link-menus" href="#">Acerca de Nosotros</a></li>
+                            <li><a id="link-menus" href="#">Servicios</a></li>
+                            <li><a id="link-menus" href="#">Colaboradores</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4"> 
+                    <div class="contactos">
+                        <h5>Para mas informacion</h5>
+                        <p><span style="color: var(--text-color-span);">Contactanos</span></p>
+                    </div>
+                    <form class="form-footer">
+                        <div class="mb-1">
+                            <label for="formulario-footer" class="label-footer">Nombre</label>
+                            <input type="text" id="formulario-footer" class="input-footer" style="color: white">
+                        </div>
+                        <div class="mb-1">
+                            <label for="formulario-footer" class="label-footer">Correo Electronico</label>
+                            <input type="email" id="formulario-footer" class="input-footer" style="color: white">
+                        </div>
+                        <div class="mb-1">
+                            <input type="text" id="formulario-footer" class="input-footer" placeholder="Escriba su mensaje..." style="color: white; padding: 10px">
+                        </div>
+                        <div class="mt-3">
+                            <button type="submit" class="btn btn-outline-light">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="copyright" style="text-align: center; padding-top: 10px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>© <?php echo date('Y');?> Todos los derechos Reservados. <a href="#">EsAlex's</a></p>
+                    </div>
                 </div>
             </div>
         </div>
